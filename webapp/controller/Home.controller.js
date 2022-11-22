849ab794f34710d5c9c2194a68ddb6e8a342900e
Shad Musthafa <shad.musthafa@stellium.com>
2022-11-22 17:15:59 +0530

    Models - i18n and aggregation binding

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -4,19 +4,15 @@ sap.ui.define([
     return BaseController.extend("com.stel.myUI5App.myui5app.controller.Home", {
         /** Lifecycle hooks in sapui5 */
         onInit: function () {
-            console.clear();
-            var oModelData = {
-                name: "John Smith",
-                isFooterVisible: false
-            };
-            // var oModel = new sap.ui.model.json.JSONModel("../model/viewModelData.json");
-            var oModel = new sap.ui.model.json.JSONModel(oModelData);
-            oModel.setDefaultBindingMode("OneWay");
-
+            // console.clear();
+            // var oModelData = {
+            //     name: "John Smith",
+            //     isFooterVisible: false
+            // };
+            // var oModel = new sap.ui.model.json.JSONModel(oModelData);
+            // oModel.setDefaultBindingMode("OneWay");
+            
             // //Tell the view the model is available
-            var oView = this.getView();
-            oView.setModel(oModel, "viewModel");
-
             // console.log("View Object", oView);
 
             // Tell UI5 core that model is available
@@ -24,9 +20,11 @@ sap.ui.define([
             // oCore.setModel(oModel, "viewModel");
             // console.log("Core",this.getCore());
 
+            var oModel = new sap.ui.model.json.JSONModel("../model/viewModelData.json");
+            var oView = this.getView();
+            oView.setModel(oModel, "viewModel");
             console.log(oModel);
         },
-
 
         /** Event Handlers */
         sayHelloBtnPress: function () {
@@ -37,7 +35,8 @@ sap.ui.define([
             var oView = this.getView(),
                 oModel = oView.getModel("viewModel");
             oModel.setProperty("/isFooterVisible", true);
-
+            // var oProp = oModel.getProperty("/isFooterVisible");
+            // var oModelData = oModel.getData();
         },
 
         onHideFooterBtnPress: function () {
